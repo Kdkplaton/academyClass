7c64a4ce629da8751071bcd07a77943c651d849c
Kdkplaton <kdkplaton@naver.com>
2025-01-14 19:44:21 +0900

    0114_클래스

---
+++ Class_1th_Stream/Class_1th_Stream.cpp
@@ -0,0 +1,85 @@
+﻿// Program.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+using namespace std;
+
+int data = 1;
+
+int main()
+{
+#pragma region 스트림
+	// 시간의 흐름에 따라 연속적으로 발생하는 데이터의 흐름
+
+	// << (삽입 연산자)
+	// 자신이 참조하고 있는 값을 반환하여 출력하는 연산자
+
+	/*
+	int errorCode = -9999;
+	cout << "Hello World!" << endl;
+	cout << "에러코드는 " << errorCode << " 입니다.";
+	*/
+
+	// >> (추출 연산자)
+	// 특정한 값을 입력받은 다음 버퍼에 저장하는 연산자
+
+	/*
+	int n = 0;
+	cin >> n;
+	cout << "n의 값: " << n << endl;
+	*/
+
+#pragma endregion
+
+#pragma region 범위 지정 연산자
+	// 여러 범위에서 사용되는 식별자를 구분하는데 사용하는 연산자
+
+	// 범위 지정 연산자는 전역 변수와 같은 이름의 지역 변수가 선언되었을때,
+	// 가장 가까운 범위에 선언된 변수의 이름을 사용하는 범위 규칙이 존재하기 때문에 전역 변수가 호출되지 않음
+
+	/*
+	int data = 10;
+	cout << "지역변수 data의 값: " << data << endl;
+	cout << "전역변수 data의 값: " << ::data << endl;
+	*/
+
+#pragma endregion
+
+#pragma region 동적 할당
+	// 프로그램을 실행 중에 필요한 만큼 메모리를 할당하는 작업
+
+	// 64bit 운영체제에서 포인터의 크기 : 8 Byte
+
+	/*int* p = new int;
+	*p = 100;
+	cout << "*p's Value: " << *p << endl;
+	cout << "p's Address Value: " << p << endl;
+
+	delete p;
+
+	p = new int[3];
+	for (int i = 0; i < 3; i++) {
+		*(p + i) = 10 + (i * 10);
+	}
+
+	for (int i = 0; i < 3; i++) {
+		cout << "p의 " << i+1 << " 번째 값: " << p[i] << endl;
+	}
+	cout << "p[0]의 Address Value: " << &(p[0]) << endl;
+	cout << "p의 Address Value: " << p << endl;
+
+	delete[ ] p;*/
+
+#pragma endregion
+
+
+}
+
+// 메모리영역 4가지
+
+
+// 스택메모리
+
+
+// 힙메모리
+
--- Program/Program.cpp
+++ Program/Program.cpp
@@ -4,78 +4,116 @@
 #include <iostream>
 using namespace std;
 
-int data = 1;
+class GameObject {
+#pragma region 접근 지정자
+	// 클래스 내부에 포함되어 있는 속성에 접근 범위를 제한하는 지정자
+
+	// public : 클래스 내부와 자기가 상속하고 있는 클래스 그리고 클래스 외부에서도 접근을 허용하는 지정자
+	// protected : 클래스 내부와 자기가 상속하고 있는 클래스까지만 접근을 허용하는 지정자
+	// private : 클래스 내부만 접근을 허용하는 지정자
+	
+
+
+#pragma endregion
+
+private:
+	int x;
+protected:
+	int y;
+public:
+	int z;
+
+	int Z() {
+		return z;
+	}
+
+	// 생성자와 소멸자 정의
+	GameObject() {
+		x = 0;
+		y = 1;
+		z = 2;
+	}
+	~GameObject() {
+
+	}
+
+	// 반환자 및 지정자 (get & set) 정의
+	int getX() {
+		return this->x;
+	}
+	void setZ(int value) {
+		this->z = value;
+	}
+
+
+};
+
+void Swap1(int x, int y) {
+	int n = x;
+	x = y;
+	y = n;
+}
+void Swap2(int& x, int& y) {
+	int n = x;
+	x = y;
+	y = n;
+}
 
 int main()
 {
-#pragma region 스트림
-	// 시간의 흐름에 따라 연속적으로 발생하는 데이터의 흐름
+#pragma region 클래스
+	// 사용자 정의 데이터 유형으로 속성과 함수가 포함되어있으며,
+	// 클래스를 통해 객체를 생성하여 접근하고 사용하는 집합체
 
-	// << (삽입 연산자)
-	// 자신이 참조하고 있는 값을 반환하여 출력하는 연산자
+	// 클래스의 경우 클래스 내부에 있는 변수는 클래스의 메모리 영역에 포함되지만,
+	// 정적 변수와 함수의 메모리는 클래스 영역에 포함되지 않음
 
-	/*
-	int errorCode = -9999;
-	cout << "Hello World!" << endl;
-	cout << "에러코드는 " << errorCode << " 입니다.";
-	*/
+	// !! 클래스 내의 변수는 기본적으로 (접근지정자 미지정시) private 속성 !!
 
-	// >> (추출 연산자)
-	// 특정한 값을 입력받은 다음 버퍼에 저장하는 연산자
+	/*GameObject gameObject1, gameObject2, gameObject3;
+	
+	gameObject1.setZ(5);
+	gameObject2.setZ(10);
+	gameObject3.setZ(20);
 
-	/*
-	int n = 0;
-	cin >> n;
-	cout << "n의 값: " << n << endl;
-	*/
+	cout << "gameObject1의 z값: " << gameObject1.Z() << endl;
+	cout << "gameObject2의 z값: " << gameObject2.Z() << endl;
+	cout << "gameObject3의 z값: " << gameObject3.Z() << endl;*/
 
 #pragma endregion
 
-#pragma region 범위 지정 연산자
-	// 여러 범위에서 사용되는 식별자를 구분하는데 사용하는 연산자
+#pragma region 참조자
+	// 어떤 변수의 메모리 공간에 다른 이름을 지정하는 지정자
 
-	// 범위 지정 연산자는 전역 변수와 같은 이름의 지역 변수가 선언되었을때,
-	// 가장 가까운 범위에 선언된 변수의 이름을 사용하는 범위 규칙이 존재하기 때문에 전역 변수가 호출되지 않음
+	/*int data = 10;
+	int& reference = data;
 
-	/*
-	int data = 10;
-	cout << "지역변수 data의 값: " << data << endl;
-	cout << "전역변수 data의 값: " << ::data << endl;
-	*/
+	cout << "data의 값: " << data << endl;
+	cout << "reference의 값: " << reference << endl;
 
-#pragma endregion
+	reference = 20;
+	cout << "data의 값: " << data << endl;
+	cout << "reference의 값: " << reference << endl;*/
 
-#pragma region 동적 할당
-	// 프로그램을 실행 중에 필요한 만큼 메모리를 할당하는 작업
+	int a = 111;
+	int b = 222;
 
-	// 64bit 운영체제에서 포인터의 크기 : 8 Byte
+	Swap1(a, b);
+	cout << "Swap1 (Call by Value)" << endl;
+	cout << "a의 값: " << a << endl;
+	cout << "b의 값: " << b << endl;
 
-	int* p = new int;
-	*p = 100;
-	cout << "*p's Value: " << *p << endl;
-	cout << "p's Address Value: " << p << endl;
-
-	delete p;
-
-	p = new int[3];
-	for (int i = 0; i < 3; i++) {
-		*(p + i) = 10 + (i * 10);
-	}
-
-	for (int i = 0; i < 3; i++) {
-		cout << "p의 " << i+1 << " 번째 값: " << p[i] << endl;
-	}
-	cout << "p[0]의 Address Value: " << &(p[0]) << endl;
-	cout << "p의 Address Value: " << p << endl;
-
-	delete[ ] p;
+	Swap2(a, b);
+	cout << "Swap2 (Call by Reference)" << endl;
+	cout << "a의 값: " << a << endl;
+	cout << "b의 값: " << b << endl;
 
 #pragma endregion
+	
 
 }
 
-// 스택메모리
+// C언어 -> 구조체 (값타입?)
+// C++ -> 클래스 (참조타입) 멀티-패러다임 언어
 
-
-// 힙메모리
 
