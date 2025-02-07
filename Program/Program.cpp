deade73d75d285fc0ffca205540532de221dcb83
Kdkplaton <kdkplaton@naver.com>
2025-02-07 19:43:46 +0900

    0207_컨테이너어댑터

--- Program/Program.cpp
+++ Program/Program.cpp
@@ -1,88 +1,33 @@
 ﻿#include <iostream>
-#include <vector>
+#include <stack>
 
 using namespace std;
 
 int main() {
-#pragma region STL ()
-	// 선형 컨테이너
-	// vector, list, deque, string
+#pragma region Stack
+	stack<int> stk1;
 
-	// 컨테이너 어댑터
-	// stack, queue, priority_queue 
+	cout << "stack.push 실행" << endl;
+	for (int i = 1; i <= 5; i++) { stk1.push(10 * i); }
 
-	// 연관 컨테이너
-	//
+	cout << "stack.size(): " << stk1.size() << endl;
+	cout << endl;
 
+	cout << "stack.pop 실행" << endl;
+	while(stk1.empty() == false) { 
+		cout << "stk1.top(): " << stk1.top() << endl;
+		stk1.pop();
+	}
+
+	cout << "stack.size(): " << stk1.size() << endl;
+
+#pragma endregion
+
+#pragma region Queue?
 
 
 #pragma endregion
 
-#pragma region Vector Container
-	vector<int> v1;
-	v1.reserve(5);
-
-	for (int i = 1; i <= 5; i++) {
-		v1.push_back(10*i);
-	}
-
-	cout << "v1.size(): " << v1.size() << endl;
-	cout << "v1.capacity(): " << v1.capacity() << endl;
-	for (int i = 0; i < v1.size(); i++) {
-		cout << "v1[" << i << "]: " << v1[i] << endl;
-	}
-
-	cout << endl;
-
-	v1.push_back(60);
-	cout << "v1.size(): " << v1.size() << endl;
-	cout << "v1.capacity(): " << v1.capacity() << endl;
-	for (int i = 0; i < v1.size(); i++) {
-		cout << "v1[" << i << "]: " << v1[i] << endl;
-	}
-
-	cout << endl;
-
-	// 맨 뒤의 데이터 삭제
-	v1.pop_back();
-	cout << "v1.size(): " << v1.size() << endl;
-	cout << "v1.capacity(): " << v1.capacity() << endl;
-	for (int i = 0; i < v1.size(); i++) {
-		cout << "v1[" << i << "]: " << v1[i] << endl;
-	}
-
-	cout << endl;
-
-	v1.reserve(5);
-	// 첫번째 것도 삭제 가능!
-	v1.erase(v1.begin());
-	cout << "v1.size(): " << v1.size() << endl;
-	cout << "v1.capacity(): " << v1.capacity() << endl;
-	for (int i = 0; i < v1.size(); i++) {
-		cout << "v1[" << i << "]: " << v1[i] << endl;
-	}
-
-	cout << endl;
-
-	v1.push_back(70);
-	v1.push_back(80);
-	cout << "v1.size(): " << v1.size() << endl;
-	cout << "v1.capacity(): " << v1.capacity() << endl;
-	for (int i = 0; i < v1.size(); i++) {
-		cout << "v1[" << i << "]: " << v1[i] << endl;
-	}
-
-	cout << endl;
-
-	v1.front() = v1.front() + 5;
-	for (int i = 0; i < v1.size(); i++) {
-		cout << "v1[" << i << "]: " << v1[i] << endl;
-	}
-
-#pragma endregion
-
-
-	
 
 
 	return 0;
