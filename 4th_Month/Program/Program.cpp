e95f0ccf7135b06d74164683ea7166a72b6f35c4
Kdkplaton <kdkplaton@naver.com>
2025-02-28 19:44:42 +0900

    0228_편향 이진트리

--- 4th_Month/Program/Program.cpp
+++ 4th_Month/Program/Program.cpp
@@ -1,34 +1,7 @@
 ﻿#include <iostream>
 using namespace std;
 
-template <typename T>
-struct hashTable {
-private:
-	char** key;
-	T* data;
-	int size;
-
-public:
-	hashTable(int max = 5) {
-		this->size = 0;
-		this->key = new char* [max];
-		this->data = new T[max];
-	}
-	~hashTable() {
-		delete key;
-		delete data;
-	}
-
-	void Add(char* key, T* data) {
-		for (int i = 0; i < size; i++) {
-			if (this->key[i] = key) {}
-			else {}
-		}
-
-
-	}
-
-};
+static unique_ptr<const char*> u_ptr;
 
 struct String {
 private:
@@ -70,7 +43,28 @@ public:
 		temp = nullptr;			// 복사에 사용한 포인터가 지정하는 메모리 변경
 		delete temp;			// 복사에 사용한 포인터 해제 (안해도 됨?)
 	}
-	void operator + (const char* target) {
+	const char* operator + (const char* target) {
+		int addLength = getLen(target);
+		char* temp = new char[this->size + addLength];
+		temp[0] = '\0';
+
+		int idx = 0;
+		while (this->string[idx] != '\0') { temp[idx] = this->string[idx++]; }
+		int endPoint = idx;
+
+		idx = 0;
+		while (target[idx] != '\0') { temp[endPoint + idx] = target[idx++]; }
+		temp[endPoint + idx] = '\0';
+
+		int totalLength = this->size + addLength;
+		
+		u_ptr = make_unique<const char*>(&(temp[0]));
+		temp = nullptr;
+		delete[] temp;
+		return &(*u_ptr)[0];
+	}
+	void Append(const char* target) {
+		// 임시 포인터로 메모리 생성
 		int addLength = getLen(target);
 		char* temp = new char[this->size + addLength];
 
@@ -88,16 +82,27 @@ public:
 		}
 		temp[endPoint + idx] = '\0';
 
-		// 크기 변경
+		// 변경된 크기로 재설정
 		int totalLength = getLen(temp);
 		this->size = totalLength + 1;
 
+		// 완성된 메모리로 지정 변경
 		delete[] this->string;
 		this->string = temp;
 
+		// 사용한 포인터 해제
 		temp = nullptr;
 		delete temp;
+	}
 
+	int Find(const char* input) {
+		int res = -1;
+		int length = getLen(input);
+
+
+
+
+		return res;
 	}
 
 	const char* getStr() { return this->string; }
@@ -109,7 +114,7 @@ public:
 int main() {
 #pragma region 문자열(String)
 	String str1;
-	str1 = "Hello World!";
+	str1 = "Hello World 1!";
 	
 	str1.printStr();
 	cout << "문자열 크기: " << str1.Size() << endl;
@@ -117,8 +122,17 @@ int main() {
 	
 	cout << endl;
 
+	str1 = "Hello ";
+	str1 = str1 + "World 2!";
+	// cout << "u_ptr: " << *u_ptr << endl;
+	str1.printStr();
+	cout << "문자열 크기: " << str1.Size() << endl;
+	cout << "문자열 길이: " << str1.getLen(str1.getStr()) << endl;
+
+	cout << endl;
+
 	str1 = "Hello";
-	str1 + "World!";
+	str1.Append("World 3!");
 	str1.printStr();
 	cout << "문자열 크기: " << str1.Size() << endl;
 	cout << "문자열 길이: " << str1.getLen(str1.getStr()) << endl;
