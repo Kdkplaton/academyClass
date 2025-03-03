cfb63efd918a03dba0300422fa57f6875dcc6281
Kdkplaton <kdkplaton@naver.com>
2025-03-04 01:05:41 +0900

    0303_완전이진트리

---
+++ 4th_Month/Class_8th_String/source.cpp
@@ -0,0 +1,160 @@
+﻿#include <iostream>
+using namespace std;
+
+static unique_ptr<const char*> u_ptr;
+
+struct String {
+private:
+	char* string;
+	int size;
+public:
+	String(char* input = nullptr) {
+		this->string = input;
+		this->size = 0;
+	}
+	~String() {
+		delete this->string;
+	}
+
+	int getLen(const char* target) {
+		int count = 0;
+		while (target[count] != '\0') { count++; }
+
+		return count;
+	}
+
+	void operator = (const char* input) {
+		// 1. 복사할 문자열 크기 구하기 & 메모리 생성
+		int length = getLen(input);		// 문자열 길이 구하기
+		this->size = length + 1;					// 문자열 크기 설정
+		char* temp = new char[this->size];		// 설정된 크기로 메모리 생성
+
+		// 2. 생성된 메모리에 문자열 복사
+		int idx = 0;
+		while (input[idx] != '\0') {
+			temp[idx] = input[idx++];
+		}
+		temp[idx] = '\0';		// 문자열에 '\0'는 복사되지 않았으므로 '\0'도 복사
+
+		// 3. 기존 메모리 해제 및 포인터 지정
+		delete[] this->string;	// 기존 메모리 할당 해제
+		this->string = temp;	// 복사된 메모리 지정
+
+		temp = nullptr;			// 복사에 사용한 포인터가 지정하는 메모리 변경
+		delete temp;			// 복사에 사용한 포인터 해제 (안해도 됨?)
+	}
+	const char* operator + (const char* target) {
+		int addLength = getLen(target);
+		char* temp = new char[this->size + addLength];
+		temp[0] = '\0';
+
+		int idx = 0;
+		while (this->string[idx] != '\0') { temp[idx] = this->string[idx++]; }
+		int endPoint = idx;
+
+		idx = 0;
+		while (target[idx] != '\0') { temp[endPoint + idx] = target[idx++]; }
+		temp[endPoint + idx] = '\0';
+
+		int totalLength = this->size + addLength;
+
+		u_ptr = make_unique<const char*>(&(temp[0]));
+		temp = nullptr;
+		delete[] temp;
+		return &(*u_ptr)[0];
+	}
+	void Append(const char* target) {
+		// 임시 포인터로 메모리 생성
+		int addLength = getLen(target);
+		char* temp = new char[this->size + addLength];
+
+		// 기존 문자열
+		int idx = 0;
+		while (this->string[idx] != '\0') {
+			temp[idx] = this->string[idx++];
+		}
+		int endPoint = idx;
+
+		// 추가할 문자열
+		idx = 0;
+		while (target[idx] != '\0') {
+			temp[endPoint + idx] = target[idx++];
+		}
+		temp[endPoint + idx] = '\0';
+
+		// 변경된 크기로 재설정
+		int totalLength = getLen(temp);
+		this->size = totalLength + 1;
+
+		// 완성된 메모리로 지정 변경
+		delete[] this->string;
+		this->string = temp;
+
+		// 사용한 포인터 해제
+		temp = nullptr;
+		delete temp;
+	}
+
+	int Find(const char* input) {
+		int res = -1;		// 결과값 : 조회 실패시 -1 반환
+		int length = getLen(input);		// 입력된 문자열의 길이
+
+		for (int i = 0; i < (this->size - length); i++) {		// 총 길이에서 검색할 문자열의 길이 만큼 빼고 반복
+			if (this->string[i] == input[0]) {					// 시작점(으로 추정되는 지점) 발견시
+				res = i;										// 발견 지점에 시작점 등록
+				for (int j = 0; j < length; j++) {				// 확인절차 진행
+					if (this->string[i + j] == input[j]) {}		// 해당 지점부터 비교
+					else { res = -1; break; }					// 일치하지 않는다면 발견지점 -1(미발견) 설정
+				}
+			}
+			else { continue; }		// 시작추정지점 발견할때까지 통과
+		}
+
+		return res;		// 반환값은 index값, 없으면 -1 반환
+	}
+
+	const char* getStr() { return this->string; }
+	void printStr() { cout << this->string << endl; }
+	int Size() { return this->size; }
+
+};
+
+int main() {
+#pragma region 문자열(String)
+	String str1;
+	str1 = "Hello World 1!";
+
+	str1.printStr();
+	cout << "문자열 크기: " << str1.Size() << endl;
+	cout << "문자열 길이: " << str1.getLen(str1.getStr()) << endl;
+
+	cout << endl;
+
+	str1 = "Hello ";
+	str1 = str1 + "World 2!";
+	str1.printStr();
+	cout << "문자열 크기: " << str1.Size() << endl;
+	cout << "문자열 길이: " << str1.getLen(str1.getStr()) << endl;
+
+	cout << endl;
+
+	str1 = "Hello";
+	str1.Append("World 3!");
+	str1.printStr();
+	cout << "문자열 크기: " << str1.Size() << endl;
+	cout << "문자열 길이: " << str1.getLen(str1.getStr()) << endl;
+
+	cout << endl;
+
+	str1.printStr();
+	cout << "\"el\" 검색결과: " << str1.Find("el") << endl;
+	cout << "\"rl\" 검색결과: " << str1.Find("rl") << endl;
+	cout << "\"ld\" 검색결과: " << str1.Find("ld") << endl;
+	cout << "\"hel\" 검색결과: " << str1.Find("hel") << endl;
+
+	cout << endl;
+
+#pragma endregion
+
+	return 0;
+}
--- 4th_Month/Program/Program.cpp
+++ 4th_Month/Program/Program.cpp
@@ -1,145 +1,157 @@
 ﻿#include <iostream>
 using namespace std;
 
-static unique_ptr<const char*> u_ptr;
+// c#, Java의 딕셔너리가 이것과 (거의) 같음
+template <typename KEY, typename VALUE>
+class HashTable {
+	struct Node {
+	protected:
+		KEY key;
+		VALUE value;
+		Bucket* next;
+	public:
+		Node(KEY key = NULL, VALUE value = NULL) {
+			this->key = key;
+			this->value = value;
+			this->next = nullptr;
+		}
+		~Node() {
 
-struct String {
+		}
+
+		KEY getKey() { return this->key; }
+		void setKey(KEY input) { this->key = input; }
+		VALUE getValue() { return this->value; }
+		void setValue(VALUE input) { this->value = input; }
+		Node* getNext() { return this->next; }
+		void setNext(Node* newNode) { this->next = newNode; }
+	};
+
+	class Bucket {
+		Node* head;
+		int count;
+	public:
+		Bucket() {
+			this->head = nullptr;
+			this->count = 0;
+		}
+		~Bucket() {
+			while (this->count > 0) { pop_front(); }
+		}
+
+		void pop_front() {
+			Node* temp = this->head;
+			this->head = this->head->getNext();
+			delete temp;
+			this->count--;
+		}
+
+
+		void addNode(Node* newNode) {
+			Node* cur = this->head;
+			while (cur->getNext() != nullptr) { cur = cur->getNext(); }
+
+			cur->getNext(newNode);
+		}
+		int getCount() { return this->count; }
+	};
+
 private:
-	char* string;
-	int size;
+	Bucket* buckets;
+	int size, count;
 public:
-	String(char* input = nullptr) {
-		this->string = input;
-		this->size = 0;
+	HashTable(int size = 6) {
+		this->buckets = new Bucket[size];
+		this->size = size;
+		this->count = 0;
 	}
-	~String() {
-		delete this->string;
-	}
-
-	int getLen(const char* target) {
-		int count = 0;
-		while (target[count] != '\0') { count++; }
-
-		return count;
+	~HashTable() {
+		// delete key, value;
 	}
 
-	void operator = (const char* input) {
-		// 1. 복사할 문자열 크기 구하기 & 메모리 생성
-		int length = getLen(input);		// 문자열 길이 구하기
-		this->size = length +1;					// 문자열 크기 설정
-		char* temp = new char[this->size];		// 설정된 크기로 메모리 생성
+	template <typename T>
+	int HashFunction(T key) {
+		int res, min = 0;
 
-		// 2. 생성된 메모리에 문자열 복사
-		int idx = 0;
-		while (input[idx] != '\0') {
-			temp[idx] = input[idx++];
+		for (int i = 0; i < this->size; i++) {
+			if(min > this->buckets[i]->count)
 		}
-		temp[idx] = '\0';		// 문자열에 '\0'는 복사되지 않았으므로 '\0'도 복사
-
-		// 3. 기존 메모리 해제 및 포인터 지정
-		delete[] this->string;	// 기존 메모리 할당 해제
-		this->string = temp;	// 복사된 메모리 지정
-
-		temp = nullptr;			// 복사에 사용한 포인터가 지정하는 메모리 변경
-		delete temp;			// 복사에 사용한 포인터 해제 (안해도 됨?)
-	}
-	const char* operator + (const char* target) {
-		int addLength = getLen(target);
-		char* temp = new char[this->size + addLength];
-		temp[0] = '\0';
-
-		int idx = 0;
-		while (this->string[idx] != '\0') { temp[idx] = this->string[idx++]; }
-		int endPoint = idx;
-
-		idx = 0;
-		while (target[idx] != '\0') { temp[endPoint + idx] = target[idx++]; }
-		temp[endPoint + idx] = '\0';
-
-		int totalLength = this->size + addLength;
-		
-		u_ptr = make_unique<const char*>(&(temp[0]));
-		temp = nullptr;
-		delete[] temp;
-		return &(*u_ptr)[0];
-	}
-	void Append(const char* target) {
-		// 임시 포인터로 메모리 생성
-		int addLength = getLen(target);
-		char* temp = new char[this->size + addLength];
-
-		// 기존 문자열
-		int idx = 0;
-		while (this->string[idx] != '\0') {
-			temp[idx] = this->string[idx++];
-		}
-		int endPoint = idx;
-
-		// 추가할 문자열
-		idx = 0;
-		while (target[idx] != '\0') {
-			temp[endPoint + idx] = target[idx++];
-		}
-		temp[endPoint + idx] = '\0';
-
-		// 변경된 크기로 재설정
-		int totalLength = getLen(temp);
-		this->size = totalLength + 1;
-
-		// 완성된 메모리로 지정 변경
-		delete[] this->string;
-		this->string = temp;
-
-		// 사용한 포인터 해제
-		temp = nullptr;
-		delete temp;
-	}
-
-	int Find(const char* input) {
-		int res = -1;
-		int length = getLen(input);
-
-
 
 
 		return res;
 	}
 
-	const char* getStr() { return this->string; }
-	void printStr() { cout << this->string << endl; }
-	int Size() { return this->size; }
-	
+
+	void Update(KEY k_Input, VALUE v_Input) {
+		int isNew = true;
+		Bucket* cursor = this->head;
+
+		// 기존에 등록된 키(key) 인지 확인
+		while (cursor->getNext() != nullptr) {
+			if (cursor->getKey() == k_Input)
+			{
+				cursor->setValue(v_Input); isNew = false; break;
+			}
+			else { cursor = cursor->getNext(); }
+		}
+		// 미 감지 시점에서 cursor는 노드체인의 끝을 가리키고 있음
+		
+		// 기존에 등록된 키가 아니라면 추가
+		if (isNew == true) {
+			// 용량(size) 최대치 도달시 추가 실패
+			if (this->count == this->size) {
+				cout << "!! 테이블 용량 한계 !!" << endl;
+				return;
+			}
+			// 용량이 남았을 경우 추가
+			else {
+				Bucket* newNode = new Bucket(k_Input, v_Input);
+				cursor->next = newNode;
+				
+				newNode = nullptr;    delete newNode;
+			}
+
+		}
+	}
+
+	void printAll() {
+		if (this->count == 0) { cout << "테이블이 비어있음!" << endl; }
+		else {
+			for (int i = 0; i < this->size; i++) {
+				Bucket* cur = this->buckets[i];
+				cout << i + 1 << "번째 Bucket" << endl;
+
+				int counter = 0;
+				while (cur != nullptr) {
+					cout << counter + 1 << "번째 키[" << cur->getKey() << "]:값[" << cur->getValue() << "]" << endl;
+
+					counter++;
+					cur = cur->getNext();
+				}
+			}
+		}
+		cout << endl;
+	}
 };
 
+
 int main() {
-#pragma region 문자열(String)
-	String str1;
-	str1 = "Hello World 1!";
+#pragma region 해시 테이블
+	HashTable<string,int> hashTable;
+	hashTable.printAll();
+
+	hashTable.Update("Apple", 10);
+	hashTable.Update("Banana", 20);
+	hashTable.printAll();
 	
-	str1.printStr();
-	cout << "문자열 크기: " << str1.Size() << endl;
-	cout << "문자열 길이: " << str1.getLen(str1.getStr()) << endl;
-	
-	cout << endl;
-
-	str1 = "Hello ";
-	str1 = str1 + "World 2!";
-	// cout << "u_ptr: " << *u_ptr << endl;
-	str1.printStr();
-	cout << "문자열 크기: " << str1.Size() << endl;
-	cout << "문자열 길이: " << str1.getLen(str1.getStr()) << endl;
-
-	cout << endl;
-
-	str1 = "Hello";
-	str1.Append("World 3!");
-	str1.printStr();
-	cout << "문자열 크기: " << str1.Size() << endl;
-	cout << "문자열 길이: " << str1.getLen(str1.getStr()) << endl;
-
-	cout << endl;
+	hashTable.Update("Candy", 30);
+	hashTable.Update("Apple", 40);
+	hashTable.printAll();
 
 #pragma endregion
+
+
+
 
 
 	return 0;
