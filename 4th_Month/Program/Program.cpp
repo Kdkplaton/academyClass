cf0d90fdd3b8436261221ab91d1d33aeaef1842a
Kdkplaton <kdkplaton@naver.com>
2025-03-07 19:45:10 +0900

    0307_그래프_인접행렬

--- 4th_Month/Program/Program.cpp
+++ 4th_Month/Program/Program.cpp
@@ -1,142 +1,135 @@
 ﻿#include <iostream>
+#include <iomanip>
 
 using namespace std;
 
 template <typename T>
-struct priorityQueue {
+class AdjacencyMatrix {
 private:
 	T* datas;
-	int count;
+	int** links;
 	int size;
+	int count;
 public:
-	priorityQueue(int size = 20) {
-		this->size = size;
+	AdjacencyMatrix(int size = 10) {
 		this->datas = new T[size];
-		this->count = 1;
-	}
-	~priorityQueue() {
-		delete[] datas;
-	}
+		this->size = size;
+		this->count = 0;
 
-	// 부모를 기준으로 했을때,
-	// 왼쪽 자식	= (부모 * 2)
-	// 오른쪽 자식	= (부모 * 2) + 1
-
-	void Push(T data) {
-		if (this->count == this->size) { cout << "!! Error: Priority Queue Overlaod !!" << endl; }
-		else {
-			this->datas[this->count] = data;
-
-			int child = this->count;
-			int parent = child / 2;
-
-			while (child > 1) {
-				if (this->datas[parent] < this->datas[child]) {
-					T temp = this->datas[parent];
-					this->datas[parent] = this->datas[child];
-					this->datas[child] = temp;
-
-					child = parent;
-					parent = child / 2;
-				}
-				else { break; }
-			}
-
-			cout << "Data " << this->count++ << " Added!" << endl;
+		// links 초기화
+		this->links = new int* [size];
+		for (int i = 0; i < size; i++) {
+			this->links[i] = new int[size];
+			for (int j = 0; j < size; j++) { this->links[i][j] = -1; }
 		}
 	}
-	void Pop() {
-		int idx = 1;
-		int left = idx * 2;
-		int right = (idx * 2) + 1;
+	~AdjacencyMatrix() {
+		delete[] this->datas;
 
-		if (this->count == 0) { cout << "!! Error: Priority Queue Empty !!" << endl; }
+		for (int i = 0; i < this->size; i++) { delete[] this->links[i]; }
+		delete[] this->links;
+	}
+
+	void addData(T data) {
+		if (this->count == this->size) { cout << "!! Error : Matrix Overflow !!" << endl; }
 		else {
-			isEmpty(left);
-			isEmpty(right);
+			this->datas[this->count++] = data;
+			for (int i = 0; i < this->size; i++)
+			{ this->links[this->count - 1][i] = 0; }
+			cout << "데이터 추가 됨!" << endl;
+		}
+	}
+	void deleteData(int target) {
+		if (this->links[target][0] == -1) { cout << "!! Error : 해당 위치에 데이터가 없음 !!" << endl; }
+		else {
 
-			if (this->datas[idx*2] > this->datas[(idx*2)+1]) {
-				this->datas[idx] = this->datas[idx * 2];
-				this->datas[idx * 2] = NULL;
-			}
-			else {
-				this->datas[idx] = this->datas[(idx*2) +1];
-				this->datas[(idx * 2) + 1] = NULL;
-			}
 
+			this->count--;
 		}
 		
 	}
-	void isEmpty(int idx) {
-		if (this->datas[idx] == NULL) {
-			if (this->datas[idx * 2] > this->datas[(idx * 2) + 1]) {
-				this->datas[idx] = this->datas[idx * 2];
-				this->datas[idx * 2] = NULL;
-			}
-			else {
-				this->datas[idx] = this->datas[(idx * 2) + 1];
-				this->datas[(idx * 2) + 1] = NULL;
-			}
+	void edge(int target1, int target2)
+	{
+		if (this->links[target1][0] == -1 || this->links[target2][0] == -1) {
+			cout << "!! Error : 존재하지 않는 데이터와 연결시도 !!" << endl;
+		}
+		else {
+			this->links[target1][target2]++;
+			this->links[target2][target1]++;
+			cout << target1 << " <-> " << target2 << " 연결됨!" << endl;
 		}
 	}
-
-	T Top() { return this->datas[1]; }
-	int Count() { return this->count-1; }
-	void showAll() {
-		cout << "==== Priority Queue Data List ====" << endl;
-
-		for (int i = 1; i < this->count; i++) {
-			cout << this->datas[i] << " ";
-			if (i % 5 == 0) { cout << endl; }
+	void unEdge(int target) {
+		for (int i = 0; i < this->size; i++) {
+			if (i == target) { continue; }
+			else if (this->links[i][0] == -1) { continue; }
+			else { this->links[i][target]--; }
 		}
 
-		/*
-		int sel = 1, count = 1;
-		int left = 2, right = 3;
 
-		while (count < this->count) {
-			cout << this->datas[sel] << " ";
-			if (count++ % 5 == 0) { cout << endl; }
 
-			if (this->datas[left] >= this->datas[right]) {
-				sel = left;
-
-				if (left % 2 == 0) { left = left + 1; }
-				else { left = left * 2; }
-			}
-			else {
-				sel = right;
-
-				if (right % 2 == 0) { right = right + 1; }
-				else { right = right * 2; }
-			}
-		}
-		*/
-
-		cout << endl;
 	}
-
+	void showLinkDetail() {
+		cout << "---------- [size:" << this->size << "] link 세부목록----------" << endl;
+		for (int i = 0; i < this->size; i++) {
+			for (int j = 0; j < this->size; j++) {
+				if (this->links[i][j] == -1) { cout << setw(2) << setfill(' ') << i + 1 << "->" << j + 1 << ":N "; }
+				else { cout << setw(2) << setfill(' ') << i << "->" << j << ":" << this->links[i][j] << " "; }
+			}
+			cout << endl;
+		}
+	}
+	void showLink() {
+		cout << "---- link 목록 ----" << endl;
+		for (int i = 0; i < this->count; i++) {
+			for (int j = 0; j < this->count; j++) {
+				if (this->links[i][j] == -1) { cout << "X "; }
+				else { cout << this->links[i][j] << " "; }
+			}
+			cout << endl;
+		}
+	}
 };
 
 int main() {
-#pragma region 우선순위 큐
-	priorityQueue<int> p_Queue;
-	for (int i = 0; i < 12; i++) { p_Queue.Push((i+1)*10); }
-	p_Queue.Push(150);
-	p_Queue.Push(130);
-	p_Queue.Push(140);
-	for (int i = 16; i <= 20; i++) { p_Queue.Push(i * 10); }
-	p_Queue.Push(210);
-	cout << "큐의 데이터 개수: " << p_Queue.Count() << endl;
-	cout << "큐의 top: " << p_Queue.Top() << endl;
-	p_Queue.showAll();
+#pragma region 그래프(Graph)
+	// 그래프 용어
+	// 정점 Vertex
+	// 간선: Node를 연결하는 선
 
-	p_Queue.Pop();
-	p_Queue.Pop();
-	cout << "큐의 데이터 개수: " << p_Queue.Count() << endl;
-	cout << "큐의 top: " << p_Queue.Top() << endl;
-	p_Queue.showAll();
+	// 방향 그래프
+	// 진입 차수 : 들어오는 경로의 수
+	// 진출 차수 : 나가는 경로의 수
+	// 
+	
+	// 무방향 그래프
+	// ??
+	// ??
 
+	//int array[4][4] = {
+	//	0, 1, 0, 0,
+	//	1, 0, 1, 1,
+	//	0, 1, 0, 1,
+	//	0, 1, 1, 0
+	//};
+
+
+	AdjacencyMatrix<int> AM1;
+	AM1.addData(30);
+	AM1.addData(50);
+	AM1.edge(1, 2);
+	AM1.addData(20);
+	AM1.edge(1, 2);
+	AM1.addData(10);
+	AM1.addData(40);
+	// AM1.showLinkDetail();		// 쉽게 간선관계를 이해하기 위한 함수
+	AM1.showLink();
+
+	cout << endl;
+
+	AM1.addData(60);
+	AM1.edge(4, 2);
+	AM1.showLink();
 
 #pragma endregion
 
