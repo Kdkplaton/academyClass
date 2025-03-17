97ee9d6cad5d270ab12738094cea073b05fc2179
Kdkplaton <kdkplaton@naver.com>
2025-03-17 19:44:08 +0900

    0317_거품정렬

---
+++ 5th_Month/Class_5th_InsertionSort/source.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <random>
+#include <time.h>
+using namespace std;
+
+int main() {
+#pragma region 삽입 정렬 (Insertion Sort)
+	// 데이터를 하나씩 확인하면서 이미 정렬된 부분과 비교해서 
+	// 자신의 위치를 찾아 삽입하는 방식으로 정렬하는 알고리즘
+
+	const int size = 10;
+	int datas[size], target;
+
+	srand(time(0));
+
+	cout << "정렬 전: ";
+	for (int i = 0; i < size; i++) {
+		datas[i] = rand() % 100;
+		cout << datas[i] << " ";
+	}
+	cout << endl;
+
+	int j;
+	for (int i = 1; i < size; i++) {
+		int key = datas[i];
+		for (j = i - 1; j >= 0; j--) {
+			if (key < datas[j]) { datas[j + 1] = datas[j]; }
+			else { break; }
+		}
+		datas[j + 1] = key;
+	}
+
+	cout << "정렬 후: ";
+	for (int i = 0; i < size; i++) { cout << datas[i] << " "; }
+	cout << endl;
+
+#pragma endregion
+
+	return 0;
+}
--- 5th_Month/Program/Program.cpp
+++ 5th_Month/Program/Program.cpp
@@ -1,39 +1,8 @@
 ﻿#include <iostream>
-#include <random>
-#include <time.h>
+
 using namespace std;
 
 int main() {
-#pragma region 삽입 정렬 (Insertion Sort)
-	// 데이터를 하나씩 확인하면서 이미 정렬된 부분과 비교해서 
-	// 자신의 위치를 찾아 삽입하는 방식으로 정렬하는 알고리즘
-
-	const int size = 10;
-	int datas[size], target;
-
-	srand(time(0));
-
-	cout << "정렬 전: ";
-	for (int i = 0; i < size; i++) {
-		datas[i] = rand() % 100;
-		cout << datas[i] << " ";
-	}
-	cout << endl;
-
-	// 여기에 알고리즘을 작성하시오
-	for (int i = 1; i < size; i++) {
-		int key = datas[i];
-		for (int j = i-1; j >= 0; j--) {
-			if (key < datas[j]) { datas[j + 1] = datas[j]; datas[j] = key; }
-			else { break; }
-		}
-	}
-
-	cout << "정렬 후: ";
-	for (int i = 0; i < size; i++) { cout << datas[i] << " "; }
-	cout << endl;
-
-#pragma endregion
 
 
 	return 0;
