067ee452c31d8a1990f73d4e9161527a8161ffab
Kdkplaton <kdkplaton@naver.com>
2025-03-19 19:43:51 +0900

    0319_삽입정렬

---
+++ 5th_Month/Class_9th_CountSort/source.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <time.h>
+#include <random>
+#include <cmath>
+using namespace std;
+
+int main() {
+#pragma region 계수 정렬
+	// 데이터의 값을 비교하지 않고 각 원소에 데이터가 
+	// 몇개 있는지 개수를 세어 사용한 다음 정렬하는 알고리즘
+
+	srand(time(0));
+
+	cout << "계수정렬 5회 실행" << endl;
+	for (int j = 0; j < 5; j++) {
+		int size = 10;
+		int* numbers = new int[size];
+		int counter[5] = { 0, };
+
+		cout << "숫자 목록: ";
+		for (int i = 0; i < 10; i++) {
+			numbers[i] = rand() % 5 + 1;		// 1~5 사이의 수
+			cout << numbers[i] << " ";
+		}
+		cout << endl;
+
+		for (int i = 0; i < size; i++) { counter[numbers[i] - 1]++; }
+
+		cout << "계수정렬 결과: ";
+		for (int i = 0; i < 5; i++) {
+			for (int j = 0; j < counter[i]; j++) {
+				cout << i + 1 << " ";
+			}
+		}
+		cout << endl;
+
+		delete[] numbers;
+		cout << endl;
+	}
+
+#pragma endregion
+
+
+	return 0;
+}
--- 5th_Month/Program/Program.cpp
+++ 5th_Month/Program/Program.cpp
@@ -1,41 +1,49 @@
 ﻿#include <iostream>
-#include <time.h>
 #include <random>
-#include <cmath>
+#include <time.h>
 using namespace std;
 
+int Find(int list[], int left, int right) {
+	int a, b;
+	if (left == right) {
+		return list[left];
+		cout << "End Point Reached!" << endl;
+	}
+	else {
+		a = Find(list, left, (left + right) / 2);
+		b = Find(list, (left + right) / 2 + 1, right);
+		
+		if (a > b) { return a; }
+		else { return b; }
+	}
+}
+
 int main() {
-#pragma region 계수 정렬
-	// 데이터의 값을 비교하지 않고 각 원소에 데이터가 
-	// 몇개 있는지 개수를 세어 사용한 다음 정렬하는 알고리즘
+#pragma region 분할 정복
+	// 주어진 2개 이상의 부분으로 문제를 나눈 뒤
+	// 각 부분 문제에 대한 답을 재귀 호출을 이용하여 계산한 다음,
+	// 그 답으로부터 전체 문제의 답을 계산해내는 알고리즘
+
+	// 분할(Devide): 주어진 문제를 2개 혹은 그 이상의 형식으로 나눔
+
+	// 정복 (Conqeuer): 나누어진 문제를 재귀적으로 해결해서
+	// 나누어진 문제를 더 이상 나누어서 문제가 필요없을 때 까지 계속 분할]
+
+	// 통합 (Combine): 나누어서 해결한 문제들을 통합해서 원래 문제의 해답을 생성
 
 	srand(time(0));
 
-	cout << "계수정렬 5회 실행" << endl;
+	int datas[10];
+	
 	for (int j = 0; j < 5; j++) {
-		int size = 10;
-		int* numbers = new int[size];
-		int counter[5] = { 0, };
-
-		cout << "숫자 목록: ";
+		cout << "datas: ";
 		for (int i = 0; i < 10; i++) {
-			numbers[i] = rand() % 5 + 1;		// 1~5 사이의 수
-			cout << numbers[i] << " ";
+			datas[i] = rand() % 100 + 1;
+			cout << datas[i] << " ";
 		}
 		cout << endl;
 
-		for (int i = 0; i < size; i++) { counter[numbers[i] -1]++; }
-
-		cout << "계수정렬 결과: ";
-		for (int i = 0; i < 5; i++) {
-			for (int j = 0; j < counter[i]; j++) {
-				cout << i+1 << " ";
-			}
-		}
-		cout << endl;
-
-		delete[] numbers;
-		cout << endl;
+		cout << "최대값: " << Find(datas, 0, 9) << endl;
 	}
 
 #pragma endregion
