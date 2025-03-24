e980ab1fccdb113180ce98fe5d42e2bc5fd1f571
Kdkplaton <kdkplaton@naver.com>
2025-03-24 23:56:32 +0900

    0324_퀵정렬

---
+++ 5th_Month/Class_12th_QuickSort/source.cpp
@@ -0,0 +1,55 @@
+﻿#include <iostream>
+#include <random>
+#include <time.h>
+
+using namespace std;
+
+void QuickSort(int list[], int start, int end) {
+	if (start >= end) return;
+
+	int pivot = start;
+	int left = start + 1;
+	int right = end;
+
+	while (left <= right) {
+
+		while (left <= end && list[pivot] >= list[left]) { left++; }
+		while (right > start && list[pivot] <= list[right]) { right--; }
+
+		if (left > right) { swap(list[pivot], list[right]); }
+		else { swap(list[left], list[right]); }
+	}
+
+	QuickSort(list, start, right - 1);
+	QuickSort(list, left, end);		// left >= right +1
+}
+
+int main() {
+#pragma region 퀵 정렬
+	// 기준점을 획득한 다음 기준점을 기준으로 배열을 나누고 한 쪽에는 기준점보다 
+	// 작은 값들이 위치하게 하고 다른 한 쪽에는 기준점보다 큰 값을이 위치하게 정렬
+
+	// 나누어진 하위 배열에 대해 재귀적으로 퀵 정렬을 호출하여
+	// 모든 배열이 기본 배열이 될 때까지 반복하면서 정렬하는 알고리즘
+
+	srand(time(0));
+
+	// int datas[10] = { 4,7,3,10,6,1,9,2,8,5 };
+	int datas[10];
+	for (int i = 0; i < 10; i++) { datas[i] = rand() % 10 + 1; }
+
+	cout << "Before Sort - datas: ";
+	for (int i = 0; i < 10; i++) { cout << datas[i] << " "; }
+	cout << endl;
+
+	QuickSort(datas, 0, 9);
+
+	cout << "After Sort - datas: ";
+	for (int i = 0; i < 10; i++) { cout << datas[i] << " "; }
+	cout << endl;
+
+#pragma endregion
+
+
+	return 0;
+}
--- 5th_Month/Program/Program.cpp
+++ 5th_Month/Program/Program.cpp
@@ -4,54 +4,74 @@
 
 using namespace std;
 
-void QuickSort(int list[], int start, int end) {
-	if (start >= end) return;
-	
-	int pivot = start;
-	int left = start +1;
-	int right = end;
+void merge_sort(int list[], int start, int end) {
+	int len = (end - start) + 1;
+	int mid = (end + start) / 2;
 
-	while (left <= right) {
+	if (len > 1) {
+		merge_sort(list, start, mid);
+		merge_sort(list , mid+1, end);
 
-		while (left <= end && list[pivot] >= list[left]) {
-			left++;
-		}
-		while (right > start && list[pivot] <= list[right]) {
-			right--;
+		int* temp = new int[len];
+		for (int i = 0; i < len; i++) { temp[i] = NULL; }
+		
+		int p1 = start, p2 = mid+1;
+		cout << "p1: " << p1 << " p2: " << p2 << " ";
+		
+		for (int i = 0; i < len; i++) {
+			if (list[p1] <= list[p2]) { 
+				if (p1 <= mid) { temp[i] = list[p1++]; }
+			}
+			else {
+				if (p2 <= end) { temp[i] = list[p2++]; }
+			}
 		}
 
-		if (left > right) {
-			swap(list[pivot], list[right]);
+		cout << "temp: ";
+		for (int i = 0; i < len; i++) { cout << temp[i] << " "; }
+		cout << endl;
+
+		for (int i = 0; i < len; i++) { list[start+i] = temp[i]; }
+
+		delete[] temp;
+	}
+	else {
+		if (len > 0) {
+			if (list[start] > list[end]) { swap(list[start], list[end]); }
 		}
-		else {
-			swap(list[left], list[right]);
-		}
+		else { return; }
 	}
 
-	QuickSort(list, start, right-1);
-	QuickSort(list, left, end);
+	cout << "start: " << start << " end: " << end << " ";
+	cout << "부분배열: ";
+	for (int i = start; i <= end; i++) { cout << list[i] << " "; }
+	cout << endl;
 }
 
 int main() {
-#pragma region 퀵 정렬
-	// 기준점을 획득한 다음 기준점을 기준으로 배열을 나누고 한 쪽에는 기준점보다 
-	// 작은 값들이 위치하게 하고 다른 한 쪽에는 기준점보다 큰 값을이 위치하게 정렬
+#pragma region 병합 정렬 (Merge Sort)
+	// 하나의 리스트를 두 개의 균일한 크기로 분할하고 분할된 부분 리스트를 정렬한 다음,
+	// 두 개의 정렬된 부분 리스트를 합하여 전체가 정렬된 리스트가 되게 하는 방법
 
-	// 나누어진 하위 배열에 대해 재귀적으로 퀵 정렬을 호출하여
-	// 모든 배열이 기본 배열이 될 때까지 반복하면서 정렬하는 알고리즘
+	// 1.   리스트의 길이가 0 또는 1이면 이미 정렬된 것으로 간주 함
+
+	// 2.   그렇지 않은경우
+	// 2-1. 정렬되지 않은 리스트를 절반으로 잘라 비슷한 크기의 두 부분 리스트로 나눔
+	// 2-2. 각 부분 리스트를 재귀적으로 병합 정렬을 이용하여 정렬
+	// 2-3. 두 부분 리스트를? 다시 하나의 정렬된 리스트로 병합
 
 	srand(time(0));
 
-	int datas[10] = { 4,7,3,10,6,1,9,2,8,5 };
-	// for (int i = 0; i < 10; i++) { datas[i] = rand() % 10 + 1; }
+	int datas[10];
+	for (int i = 0; i < 10; i++) { datas[i] = rand() % 100 + 1; }
 
-	cout << "Before Sort - datas: ";
+	cout << "Before Sort: ";
 	for (int i = 0; i < 10; i++) { cout << datas[i] << " "; }
 	cout << endl;
 
-	QuickSort(datas, 0, 9);
-	
-	cout << "After Sort - datas: ";
+	merge_sort(datas, 0, 9);
+
+	cout << "After  Sort: ";
 	for (int i = 0; i < 10; i++) { cout << datas[i] << " "; }
 	cout << endl;
 
