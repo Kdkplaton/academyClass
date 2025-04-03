cb5067edb92bee9b021f5bfa0a902faa551380bf
Kdkplaton <kdkplaton@naver.com>
2025-04-03 18:01:27 +0900

    0402_계수정렬

---
+++ 5th_Month/Class_19th_UnionFind/source.cpp
@@ -0,0 +1,110 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+struct UnionFind {
+private:
+	int size;
+	vector<int>* united;
+public:
+	UnionFind(vector<int> v1, vector<int> v2) {
+		int len1 = v1.size();
+		int len2 = v2.size();
+
+		this->size = len1 + len2;
+		united = new vector<int>[2];
+
+		// union 배열 초기화
+		for (int i = 0; i < len1; i++) {
+			this->united[0].push_back(v1[i]);
+			this->united[1].push_back(v1[i]);
+		}
+		for (int i = 0; i < len2; i++) {
+			this->united[0].push_back(v2[i]);
+			this->united[1].push_back(v2[i]);
+		}
+
+		// 초기화 결과
+		cout << "----- init result -----" << endl;
+		for (int i = 0; i < this->size; i++) { cout << this->united[0][i] << " "; }
+		cout << endl;
+		for (int i = 0; i < this->size; i++) { cout << this->united[1][i] << " "; }
+		cout << endl;
+	}
+	~UnionFind() {
+		delete[] united;
+		cout << "\nUnionFind 종료!" << endl;
+	}
+
+	int find(int val) {
+		for (int i = 0; i < this->size; i++) {
+			if (this->united[0][i] == val) { return this->united[1][i]; }
+		}
+	}
+	void Union(int a, int b) {
+		int temp1 = find(a);
+		int temp2 = find(b);
+
+		if (temp1 == a && temp2 == b) {
+			if (a <= b) { this->united[1][b] = a; }
+			else { { this->united[1][a] = b; } }
+
+			cout << a << "&" << b << " 연결됨!" << endl;
+		}
+		else {
+			cout << "이미 연결된 개체 존재!!" << endl; return;
+		}
+	}
+	bool same(int a, int b) {
+		if (this->united[1][a] == this->united[1][b]) return true;
+		else return false;
+	}
+
+	void showUnionFind() {
+		cout << "----- show united -----" << endl;
+		for (int i = 0; i < this->size; i++) { cout << this->united[0][i] << " "; }
+		cout << endl;
+		for (int i = 0; i < this->size; i++) { cout << this->united[1][i] << " "; }
+		cout << endl;
+	}
+
+};
+
+int main() {
+#pragma region 유니온-파인드 Union-Find
+	// 여러 노드가 존재할 때 어떤 노드가 다른 노드와 연결되어 있는 지 확인하는 알고리즘
+
+	// Union : 특정한 두 개의 노드를 같은 집합으로 합치는 연산
+	// Find : 특정한 노드가 어느 집합에 있는 지 확인하는 연산
+
+	vector<int> arr1 = { 0,1,2 };
+	vector<int> arr2 = { 3,4,5 };
+
+	UnionFind uf1(arr1, arr2);
+
+	cout << endl;
+
+	uf1.Union(0, 1);
+	cout << "1의 부모: " << uf1.find(1) << endl;
+	cout << "0의 부모: " << uf1.find(0) << endl;
+
+	uf1.Union(3, 4);
+	uf1.Union(4, 5);
+	cout << "5의 부모: " << uf1.find(5) << endl;
+	cout << "4의 부모: " << uf1.find(4) << endl;
+
+	cout << endl;
+
+	uf1.showUnionFind();
+
+	cout << endl;
+
+	cout << "0&1 same parent? : " << uf1.same(0, 1) << endl;
+	cout << "1&4 same parent? : " << uf1.same(1, 4) << endl;
+
+
+#pragma endregion
+
+	return 0;
+}
--- 5th_Month/Program/Program.cpp
+++ 5th_Month/Program/Program.cpp
@@ -3,102 +3,168 @@
 
 using namespace std;
 
-struct UnionFind {
+class Edge {
 private:
-	int size;
-	vector<int>* united;
+	int start;
+	int end;
+	int dist;
 public:
-	UnionFind(vector<int> v1, vector<int> v2) {
-		int len1 = v1.size();
-		int len2 = v2.size();
-
-		this->size = len1 + len2;
-		united = new vector<int> [2];
-
-		// union 배열 초기화
-		for (int i = 0; i < len1; i++) {
-			this->united[0].push_back(v1[i]);
-			this->united[1].push_back(v1[i]);
-		}
-		for (int i = 0; i < len2; i++) {
-			this->united[0].push_back(v2[i]);
-			this->united[1].push_back(v2[i]);
-		}
-
-		// 초기화 결과
-		cout << "----- init result -----" << endl;
-		for (int i = 0; i < this->size; i++) { cout << this->united[0][i] << " "; }
-		cout << endl;
-		for (int i = 0; i < this->size; i++) { cout << this->united[1][i] << " "; }
-		cout << endl;
+	Edge(int node1 = NULL, int node2 = NULL, int val = NULL) {
+		this->start = node1;
+		this->end = node2;
+		this->dist = val;
 	}
-	~UnionFind() {
-		delete[] united;
-		cout << "\nUnionFind 종료!" << endl;
+	~Edge() {
+
 	}
 
-	int find(int val) {
-		for (int i = 0; i < this->size; i++) {
-			if (this->united[0][i] == val) { return this->united[1][i]; }
-		}
+	void update_dist(int val) { this->dist = val; }
+	int get_start() { return this->start; }
+	int get_end() { return this->end; }
+	int get_dist() { return this->dist; }
+};
+
+class Kruskal {
+private:
+	int SIZE;
+	vector<Edge> edges;
+	int* parents;
+	int cost;
+public:
+	Kruskal(int size = 8) {
+		this->SIZE = size;
+		this->parents = new int[size];
+		for (int i = 0; i < size; i++) { this->parents[i] = i; cout << parents[i] << endl; }
+		this->cost = 0;
+
+		cout << "Kruskal Start!\n" << endl;
 	}
-	void Union(int a, int b) {
-		int temp1 = find(a);
-		int temp2 = find(b);
-
-		if (temp1 == a && temp2 == b) {
-			if (a <= b) { this->united[1][b] = a; }
-			else { { this->united[1][a] = b; } }
-
-			cout << a << "&" << b << " 연결됨!" << endl;
-		}
-		else {
-			cout << "이미 연결된 개체 존재!!" << endl; return;
-		}
-	}
-
-	void showUnionFind() {
-		cout << "----- show united -----" << endl;
-		for (int i = 0; i < this->size; i++) { cout << this->united[0][i] << " "; }
-		cout << endl;
-		for (int i = 0; i < this->size; i++) { cout << this->united[1][i] << " "; }
-		cout << endl;
+	~Kruskal() {
+		delete[] parents;
+		cout << "\nKruskal End!" << endl;
 	}
 
+	void set_edge(int node1, int node2, int val) {
+		for (int i = 0; i < this->edges.size(); i++) {
+			if (this->edges[i].get_start() == node1 && this->edges[i].get_end() == node2) {
+				// edges[i].update_dist(val);
+				cout << "Edge " << node1 << "↔" << node2 << " Already Exsists!" << endl; return;
+			}
+			else if (this->edges[i].get_start() == node2 && this->edges[i].get_end() == node1) {
+				// edges[i].update_dist(val);
+				cout << "Edge " << node1 << "↔" << node2 << " Already Exsists!" << endl; return;
+			}
+		}
+		this->edges.push_back(Edge(node1, node2, val));
+		if (node1 > node2) { this->parents[node1] = node2; }
+		else { this->parents[node1] = node2; }
+		cout << "Edge " << node1 << "↔" << node2 << " added!" << endl;
+	}
+	void sort_edges() {
+		for (int i = 0; i < this->edges.size() - 1; i++) {
+			int min = i;
+			for (int j = i + 1; j < this->edges.size(); j++) {
+				int dist1 = this->edges[min].get_dist();
+				int dist2 = this->edges[j].get_dist();
+				if (dist1 > dist2) { min = j; }
+			}
+			swap(this->edges[i], this->edges[min]);
+		}
+
+		cout << "Edges sorted!" << endl;
+		this->show_edges();
+	}
+	
+	void find_lowestDistTree() {
+		this->sort_edges();
+		
+		for (int i = 0; i < this->edges.size(); i++) {
+			int n1 = this->edges[i].get_start();
+			int n2 = this->edges[i].get_end();
+			if (n1 > n2) {
+				if () {}
+			}
+			else {
+
+			}
+
+		}
+
+	}
+
+	void show_edges() {
+		cout << "-- Show Edge List --" << endl;
+		for (int i = 0; i < this->edges.size(); i++) {
+			cout << "간선[" << this->edges[i].get_start() << "↔" << this->edges[i].get_end() << "] " << this->edges[i].get_dist() << endl;
+		}
+	}
+	void show_parents() {
+		cout << "-- Show Parents List --" << endl;
+		for (int i = 1; i < this->SIZE; i++) {
+			cout << i << "의 부모: " << this->parents[i] << endl;
+		}
+	}
 };
 
 int main() {
-#pragma region 유니온-파인드 Union-Find
-	// 여러 노드가 존재할 때 어떤 노드가 다른 노드와 연결되어 있는 지 확인하는 알고리즘
+#pragma region 최소 신장 트리
+	// 그래프의 모든 정점을 포함하면서 사이클이 존재하지 않는 부분 그래프
+	// 그래프의 모든 정점을 최소비용으로 연결하는 트리
 
-	// Union : 특정한 두 개의 노드를 같은 집합으로 합치는 연산
-	// Find : 특정한 노드가 어느 집합에 있는 지 확인하는 연산
+	// 그래프의 정점의 수가 n개일 때, 간선의 수는 n-1개 (결과)
 
-	vector<int> arr1 = { 0,1,2 };
-	vector<int> arr2 = { 3,4,5 };
+	Kruskal krsk1;
 
-	UnionFind uf1(arr1, arr2);
-
-	cout << endl;
-
-	uf1.Union(1, 0);
-	uf1.Union(0, 2);
-	cout << "1의 부모: " << uf1.find(1) << endl;
-	cout << "2의 부모: " << uf1.find(2) << endl;
-	cout << "0의 부모: " << uf1.find(0) << endl;
-
-	uf1.Union(3, 4);
-	uf1.Union(5, 3);
-	cout << "5의 부모: " << uf1.find(5) << endl;
-	cout << "4의 부모: " << uf1.find(4) << endl;
-
-	cout << endl;
-
-	uf1.showUnionFind();
+	// 존재하는 모든 간선 관계를 각 노드의 관점에서 전부 설정
+	// ※ 이미 등록된 관계는 설정(추가)되지 않음 ※
+	cout << "---- Initialize Nodes ----" << endl;
 	
+	krsk1.set_edge(1, 2, 64);
+	krsk1.set_edge(1, 4, 30);
+	krsk1.set_edge(1, 5, 19);
+	krsk1.set_edge(1, 7, 10);
+
+	krsk1.set_edge(2, 1, 64);	// 추가되지 않음
+	krsk1.set_edge(2, 4, 25);
+	krsk1.set_edge(2, 5, 61);
+
+	krsk1.set_edge(4, 1, 30);	// 추가되지 않음
+	krsk1.set_edge(4, 2, 25);	// 추가되지 않음
+	krsk1.set_edge(4, 7, 14);
+
+	krsk1.set_edge(7, 1, 10);	// 추가되지 않음
+	krsk1.set_edge(7, 4, 14);	// 추가되지 않음
+	krsk1.set_edge(7, 5, 73);
+
+	krsk1.set_edge(5, 1, 19);	// 추가되지 않음
+	krsk1.set_edge(5, 2, 61);	// 추가되지 않음
+	krsk1.set_edge(5, 3, 22);
+	krsk1.set_edge(5, 6, 48);
+	krsk1.set_edge(5, 7, 73);	// 추가되지 않음
+
+	krsk1.set_edge(3, 5, 22);	// 추가되지 않음
+	krsk1.set_edge(3, 6, 36);
+	
+	krsk1.set_edge(6, 3, 36);	// 추가되지 않음
+	krsk1.set_edge(6, 5, 48);	// 추가되지 않음
+
+	cout << "---- Initialize Ended ----" << endl;
+	
+	cout << endl;
+	
+	krsk1.show_edges();
+
+	cout << endl;
+
+	krsk1.show_parents();
+
+	cout << endl;
+
+	krsk1.find_lowestDistTree();
+
+
 
 #pragma endregion
-
 
 	return 0;
 }
