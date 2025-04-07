f756ad5f7248bd2f4cc558418947aa7d8b9eaa2f
Kdkplaton <kdkplaton@naver.com>
2025-04-07 19:34:08 +0900

    0407_퇴각탐색

---
+++ 5th_Month/Class_21th_Dijkstra/source.cpp
@@ -0,0 +1,139 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <vector>
+
+using namespace std;
+
+#define INFINITY 10000000
+
+class Dijkstra {
+private:
+	int SIZE;
+	int** weights;
+	bool* visited;
+	int* distance;
+
+public:
+	Dijkstra(int size = 7) {
+		this->SIZE = size;
+		this->weights = new int* [size];
+		for (int i = 0; i < size; i++) {
+			this->weights[i] = new int[size];
+			for (int j = 0; j < size; j++) {
+				if (i == j) { this->weights[i][j] = 0; }
+				else { this->weights[i][j] = INFINITY; }
+			}
+		}
+		this->visited = new bool[size];
+		this->distance = new int[size];
+		for (int i = 0; i < size; i++) {
+			this->visited[i] = false;
+			this->distance[i] = 0;
+		}
+
+	}
+	~Dijkstra() {
+		for (int i = 0; i < this->SIZE; i++) { delete[] this->weights[i]; }
+		delete[] this->weights;
+		delete[] visited;
+	}
+
+	void calculate(int start) {
+		int target = start - 1;
+		this->visited[target] = true;
+
+		bool check = false;
+		for (int i = 0; i < this->SIZE; i++) {
+			if (this->visited[i] == false) { check = true; break; }
+		}
+		if (check == false) {
+			cout << "Calculate End!" << endl;
+
+			cout << "Distance: ";
+			for (int i = 0; i < this->SIZE; i++) { cout << this->distance[i] << " "; }
+			cout << endl;
+
+			return;
+		}
+
+		for (int i = 0; i < this->SIZE; i++) {
+			if (this->visited[i] == true) { continue; }
+			else {
+				if (this->distance[i] >= INFINITY) { this->distance[i] = this->weights[target][i]; }
+				else { this->distance[i] += this->weights[target][i]; }
+			}
+		}
+		cout << "Distance: ";
+		for (int i = 0; i < this->SIZE; i++) { cout << this->distance[i] << " "; }
+		cout << endl;
+
+		int min = INFINITY;
+		for (int i = 0; i < this->SIZE; i++) {
+
+			if (this->visited[i] == true) { continue; }
+			else {
+				if (min > this->distance[i]) { min = this->distance[i]; }
+			}
+		}
+
+		calculate(min);
+	}
+
+	void set_weight(int x, int y, int val) {
+		if (this->weights[x - 1][y - 1] != INFINITY) { cout << "Already Exsist!!" << endl; }
+		else {
+			this->weights[x - 1][y - 1] = val; this->weights[y - 1][x - 1] = val;
+			cout << x << "↔" << y << " Set!" << endl;
+		}
+	}
+	void show_weights() {
+		for (int i = 0; i < this->SIZE; i++) {
+			for (int j = 0; j < this->SIZE; j++) {
+				cout << setw(8) << this->weights[i][j] << " ";
+			}
+			cout << endl;
+		}
+		cout << endl;
+	}
+};
+
+int main() {
+#pragma region 다익스트라 (Dijkstra)
+	// 시작점으로부터 모든 노드까지의 최소 거리를 구해주는 알고리즘
+
+	// 1. 거리 배열에 weight[시작 노드]의 값들로 초기화
+	// 2. 시작점을 방문 처리
+	// 3. 거리 배열에서 최소비용 노드를 찾고 방문 처리
+	//    단, 이미 방문한 노드는 제외
+	// 4. 최소비용 노드를 거쳐갈 지 고민하여 거리 배열을 갱신
+	//    단, 이미 방문한 노드는 제외
+	// 5. 모든 노드를 방문할 때까지 3~4번 반복
+
+	// 방문하지 않은 노드 중에서 가장 작은 거리를 가진 노드를
+	// 방문하고, 그 노드와 연결된 다른 노드까지의 거리를 계산함
+
+	Dijkstra dijkstra1;
+
+	dijkstra1.set_weight(1, 2, 2);
+	dijkstra1.set_weight(1, 3, 5);
+	dijkstra1.set_weight(1, 4, 1);
+	dijkstra1.set_weight(2, 1, 2);
+	dijkstra1.set_weight(2, 3, 3);
+	dijkstra1.set_weight(2, 4, 2);
+	dijkstra1.set_weight(3, 4, 3);
+	dijkstra1.set_weight(3, 5, 1);
+	dijkstra1.set_weight(3, 6, 5);
+	dijkstra1.set_weight(4, 2, 2);
+	dijkstra1.set_weight(4, 5, 1);
+	dijkstra1.set_weight(5, 6, 2);
+
+	dijkstra1.show_weights();
+
+	dijkstra1.calculate(1);
+
+
+#pragma endregion
+
+
+	return 0;
+}
--- 5th_Month/Program/Program.cpp
+++ 5th_Month/Program/Program.cpp
@@ -1,127 +1,50 @@
 ﻿#include <iostream>
-#include <iomanip>
-#include <vector>
 
 using namespace std;
 
-#define INFINITY 10000000
-
-class Dijkstra {
+class Backtracking {
 private:
-	int SIZE;
-	int** weights;
-	bool* visited;
-	int* distance;
+
 
 public:
-	Dijkstra(int size = 7) {
-		this->SIZE = size;
-		this->weights = new int* [size];
-		for (int i = 0; i < size; i++) {
-			this->weights[i] = new int[size];
-			for (int j = 0; j < size; j++) {
-				if (i == j) { this->weights[i][j] = 0; }
-				else { this->weights[i][j] = INFINITY; }
-			}
-		}
-		this->visited = new bool[size];
-		this->distance = new int[size];
-		for (int i = 0; i < size; i++) {
-			this->visited[i] = false;
-			this->distance[i] = 0;
-		}
+	Backtracking() {
 
 	}
-	~Dijkstra() {
-		for (int i = 0; i < this->SIZE; i++) { delete[] this->weights[i]; }
-		delete[] this->weights;
-		delete[] visited;
+	~Backtracking() {
+
 	}
 
-	void calculate(int start) {
-		bool check = false;
-		for (int i = 0; i < this->SIZE; i++) {
-			if (this->visited[i] == false) { check == true; break; }
-		}
-		if (check == false) {
-			cout << "Calculate End!" << endl;
-			
-			cout << "Distance: ";
-			for (int i = 0; i < this->SIZE; i++) { cout << this->distance[i] << ""; }
-			cout << endl;
-		}
 
-		int target = start - 1;
-		this->visited[target] = true;
-		for (int i = 0; i < this->SIZE; i++) {
-			if (this->visited[i] == true) { continue; }
-			else { this->distance[i] = this->weights[target][i]; }
-		}
 
-		int min = INFINITY;
-		for (int i = 0; i < this->SIZE; i++) {
-			if (this->visited[i] == true) { continue; }
-			else {
-				if (this->distance[min] > this->distance[i]) { min = i; }
-			}
-		}
-
-		calculate(min);
-	}
-
-	void set_weight(int x, int y, int val) {
-		if (this->weights[x - 1][y - 1] != INFINITY) { cout << "Already Exsist!!" << endl; }
-		else {
-			this->weights[x - 1][y - 1] = val; this->weights[y - 1][x - 1] = val;
-			cout << x << "↔" << y << " Set!" << endl;
-		}
-	}
-	void show_weights() {
-		for (int i = 0; i < this->SIZE; i++) {
-			for (int j = 0; j < this->SIZE; j++) {
-				cout << setw(8) << this->weights[i][j] << " ";
-			}
-			cout << endl;
-		}
-		cout << endl;
-	}
 };
 
 int main() {
-#pragma region ???
-	// 시작점으로부터 모든 노드까지의 최소 거리를 구해주는 알고리즘
+#pragma region 퇴각 검색 (Backtracking Algorithm)
+	// 해를 찾아가는 도중에 지금 경로가 해가 될 것 같지 않으면,
+	// 더 이상 깊이 들어가지 않고, 이전 단계로 다시 들어가는 알고리즘
 
-	// 1. 거리 배열에 weight[시작 노드]의 값들로 초기화
-	// 2. 시작점을 방문 처리
-	// 3. 거리 배열에서 최소비용 노드를 찾고 방문 처리
-	//    단, 이미 방문한 노드는 제외
-	// 4. 최소비용 노드를 거쳐갈 지 고민하여 거리 배열을 갱신
-	//    단, 이미 방문한 노드는 제외
-	// 5. 모든 노드를 방문할 때까지 3~4번 반복
+	// 퇴각 검색의 유망성 판단
+	// 해가 될 만한지 판단한 후에 유망하지 않다고 결정되면,
+	// 그 노드의 이전 노드로 돌아가 다음 자식 노드로 이동
+
+	// 해가 될 만한 기능이 있으면 유망함 (Promising)
+	// 유망하지 않은 노드에 가지 않는 것 (Pruning)
+
 	
-	// 방문하지 않은 노드 중에서 가장 작은 거리를 가진 노드를
-	// 방문하고, 그 노드와 연결된 다른 노드까지의 거리를 계산함
 
-	Dijkstra dijkstra1;
-
-	dijkstra1.set_weight(1, 2, 2);
-	dijkstra1.set_weight(1, 3, 5);
-	dijkstra1.set_weight(1, 4, 1);
-	dijkstra1.set_weight(2, 1, 2);
-	dijkstra1.set_weight(2, 3, 3);
-	dijkstra1.set_weight(2, 4, 2);
-	dijkstra1.set_weight(3, 4, 3);
-	dijkstra1.set_weight(3, 5, 1);
-	dijkstra1.set_weight(3, 6, 5);
-	dijkstra1.set_weight(4, 2, 2);
-	dijkstra1.set_weight(4, 5, 1);
-	dijkstra1.set_weight(5, 6, 2);
-
-	dijkstra1.show_weights();
-	// INFINITY가 출력할때 너무 길어서 -1로 임시 대체함
 
 #pragma endregion
 
+	// 버전 관리 시스템  - 분산형 (Git) / 중앙집중형 (SVN)
+
+	// 유니티 설치 버전 - 2022.3.xxf
+	// 앞자리 : 메이저 버전
+	// 중간자리 : 마이너 버전
+	// 뒷자리 : 패치 버전
+
+
+	
+	// DFS 와 퇴각검색의 차이점 조사
 
 	return 0;
 }
